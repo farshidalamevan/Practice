10e224352eed24fdbab25f98ce69df41c340e262
Farshid Alam Evan <farshid.a.evan@gmail.com>
2026-05-15 01:27:38 +0600

    Login Form

--- lib/login_form.dart
+++ lib/login_form.dart
@@ -43,51 +43,54 @@ class LoginForm extends StatelessWidget {
           color: Colors.black,
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.lock, size: 80),
-          SizedBox(height: 10),
-          TextField(
-            controller: emailController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              labelText: 'Enter your email',
-              hintText: 'name@email.com',
-              prefixIcon: Icon(Icons.email_outlined),
-              border: OutlineInputBorder(),
-              filled: true,
-              fillColor: Colors.grey.shade200,
-              // helper: Text('Enter your email')
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.lock, size: 80),
+            SizedBox(height: 10),
+            TextField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Enter your email',
+                hintText: 'name@email.com',
+                prefixIcon: Icon(Icons.email_outlined),
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                // helper: Text('Enter your email')
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            keyboardType: TextInputType.text,
-            controller: _passController,
-            decoration: InputDecoration(
-              labelText: 'Enter your password',
-              hintText: 'password',
-              prefixIcon: Icon(Icons.password),
-              border: OutlineInputBorder(),
-              filled: true,
-              fillColor: Colors.grey.shade200,
+            SizedBox(height: 20),
+            TextField(
+              keyboardType: TextInputType.text,
+              controller: _passController,
+              decoration: InputDecoration(
+                labelText: 'Enter your password',
+                hintText: 'password',
+                prefixIcon: Icon(Icons.password),
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.grey.shade200,
+              ),
+              obscureText: true,
             ),
-            obscureText: true,
-          ),
-          SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {
-              print(
-                "Email: ${emailController.text}\nPassword: ${_passController.text} ");},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green,
-              foregroundColor: Colors.white,
-              padding: EdgeInsets.only(left: 50, right: 50),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                print(
+                  "Email: ${emailController.text}\nPassword: ${_passController.text} ");},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.only(left: 50, right: 50),
+              ),
+              child: Text('Login', style: TextStyle(fontSize: 20)),
             ),
-            child: Text('Login', style: TextStyle(fontSize: 20)),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
